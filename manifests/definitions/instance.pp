b3e6346eb17bc0102f4a08145b87b9d66d5b31d2
Marc Fournier <marc.fournier@camptocamp.com>
2009-12-16 09:55:56 +0100

    tomcat::instance - fixed documentation inconsistency.

--- manifests/definitions/instance.pp
+++ manifests/definitions/instance.pp
@@ -67,12 +67,12 @@ Example usage:
   }
 
   tomcat::instance { "bar":
-    ensure         => present,
-    server_port    => 8006,
-    http_port      => 8081,
-    ajp_port       => 8010,
-    sample         => true,
-    setenv_content => [
+    ensure      => present,
+    server_port => 8006,
+    http_port   => 8081,
+    ajp_port    => 8010,
+    sample      => true,
+    setenv      => [
       'JAVA_XMX="1200m"',
       'ADD_JAVA_OPTS="-Xms128m"'
     ],
