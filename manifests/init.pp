7e9fb1f3c192c671dda220a90e22119165a21e6d
Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
2011-02-23 14:28:03 +0100

    tomcat: removed old package classes + backward compatibility

--- manifests/init.pp
+++ manifests/init.pp
@@ -19,3 +19,28 @@ class tomcat {
     default      : { notice "Unsupported operatingsystem ${operatingsystem}" }
   }
 }
+
+
+/*
+
+== Class: tomcat::package::v5-5
+
+Deprecated: use simply "include tomcat" instead!
+
+*/
+class tomcat::package::v5-5 {
+  notify {"class $name is deprecated, class 'tomcat' is automatically included for backwards compatibility":}
+  include tomcat
+}
+
+/*
+
+== Class: tomcat::package::v6
+
+Deprecated: include "tomcat" instead!
+
+*/
+class tomcat::package::v6 {
+  notify {"class $name is deprecated, class 'tomcat' is automatically included for backwards compatibility":}
+  include tomcat
+}
