4842f3d69e6c261e0a1b7f99205c40c07492a8e0
Marc Fournier <marc.fournier@camptocamp.com>
2011-11-15 13:05:24 +0100

    file renaming to be compliant with recommended module structure.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,3 @@
-import "classes/*.pp"
-import "definitions/*.pp"
-
 /*
 
 == Class: tomcat
@@ -18,54 +15,4 @@ class tomcat {
     Debian,Ubuntu: { include tomcat::debian }
     default      : { notice "Unsupported operatingsystem ${operatingsystem}" }
   }
-}
-
-/*
-
-== Class: tomcat::package::v5-5
-
-Deprecated: use simply "include tomcat" instead!
-
-*/
-class tomcat::package::v5-5 {
-  notify {"class $name is deprecated, class 'tomcat' is automatically included for backwards compatibility":}
-  include tomcat
-}
-
-/*
-
-== Class: tomcat::package::v6
-
-Deprecated: include "tomcat" instead!
-
-*/
-class tomcat::package::v6 {
-  notify {"class $name is deprecated, class 'tomcat' is automatically included for backwards compatibility":}
-  include tomcat
-}
-
-/*
-
-== Class: tomcat::v5-5
-
-Deprecated: include "tomcat::source" instead!
-
-*/
-class tomcat::v5-5 {
-  include tomcat::params
-  $tomcat_version = $tomcat::params::default_source_release_v55
-  notify {"class $name is deprecated, class 'tomcat' is automatically included for backwards compatibility":}
-  include tomcat::source
-}
-
-/*
-
-== Class: tomcat::v6
-
-Deprecated: include "tomcat::source" instead!
-
-*/
-class tomcat::v6 {
-  notify {"class $name is deprecated, class 'tomcat' is automatically included for backwards compatibility":}
-  include tomcat::source
 }
---
+++ manifests/v5-5.pp
@@ -0,0 +1,13 @@
+/*
+
+== Class: tomcat::v5-5
+
+Deprecated: include "tomcat::source" instead!
+
+*/
+class tomcat::v5-5 {
+  include tomcat::params
+  $tomcat_version = $tomcat::params::default_source_release_v55
+  notify {"class $name is deprecated, class 'tomcat' is automatically included for backwards compatibility":}
+  include tomcat::source
+}
