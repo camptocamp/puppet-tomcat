daa67e45ca3f8590bb064fcd6df37bfd17f5f835
Mickaël Canévet <mickael.canevet@camptocamp.com>
2013-04-03 10:19:03 +0200

    Allow passing $jdk_pkg to tomcat so that we can install JDK7

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,18 +1,22 @@
-/*
-
-== Class: tomcat
-
-Installs tomcat using the default version providing by distributions.
-
-Example usage:
-
-  include tomcat
-
-*/
-class tomcat {
+#== Class: tomcat
+#
+#Installs tomcat using the default version providing by distributions.
+#
+#Example usage:
+#
+#  include tomcat
+class tomcat(
+  $jdk_pkg = undef,
+) {
   case $::osfamily {
-    RedHat: { include tomcat::redhat }
-    Debian: { include tomcat::debian }
+    RedHat: {
+      validate_re($jdk_pkg, [undef, 'java-1.6.0-openjdk', 'java-1.7.0-openjdk'])
+      include tomcat::redhat
+    }
+    Debian: {
+      validate_re($jdk_pkg, [undef, 'openjdk-6-jdk', 'openjdk-7-jdk'])
+      include tomcat::debian
+    }
     default: { fail "Unsupported OS family ${::osfamily}" }
   }
 }
