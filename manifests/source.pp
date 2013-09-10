f292c5cf49c55aa46f3eb4615e138184987a0ac4
Cedric Jeanneret <cedric.jeanneret@camptocamp.com>
2013-09-10 12:01:41 +0200

    tomcat::source: linted, refactoring

--- manifests/source.pp
+++ manifests/source.pp
@@ -1,51 +1,33 @@
-/*
+# == Class: tomcat::source
+#
+# Installs tomcat using the compressed archive from your favorite tomcat
+# mirror. Files from the archive will be installed in /opt/apache-tomcat/.
+#
+# Class variables:
+# - *$log4j_conffile*: see tomcat
+#
+# Requires:
+# - java to be previously installed
+# - archive definition (from puppet camptocamp/puppet-archive module)
+# - Package["curl"]
+#
+# Tested on:
+# - RHEL 5,6
+# - Debian Lenny/Squeeze
+# - Ubuntu Lucid
+#
+# Usage:
+#   $tomcat_version = "6.0.18"
+#   include tomcat::source
+#
+class tomcat::source (
+  $version     = $tomcat::params::version,
+  $sources_src = $tomcat::params::sources_src,
+) inherits tomcat::params {
 
-== Class: tomcat::source
+  $tomcat_home = "/opt/apache-tomcat-${version}"
 
-Installs tomcat 5.5.X or 6.0.X using the compressed archive from your favorite tomcat
-mirror. Files from the archive will be installed in /opt/apache-tomcat/.
-
-Class variables:
-- *$log4j_conffile*: see tomcat
-
-Requires:
-- java to be previously installed
-- archive definition (from puppet camptocamp/puppet-archive module)
-- Package["curl"]
-
-Tested on:
-- RHEL 5,6
-- Debian Lenny/Squeeze
-- Ubuntu Lucid
-
-Usage:
-  $tomcat_version = "6.0.18"
-  include tomcat::source
-
-*/
-class tomcat::source inherits tomcat::base {
-
-  include ::tomcat::params
-
-  case $::osfamily {
-    RedHat: {
-      package { ['log4j', 'jakarta-commons-logging']:
-        ensure => present,
-      }
-    }
-    Debian: {
-      package { ['liblog4j1.2-java', 'libcommons-logging-java']:
-        ensure => present,
-      }
-    }
-    default: {
-      fail("Unsupported OS family ${::osfamily}")
-    }
-  }
-
-  $tomcat_home = "/opt/apache-tomcat-${tomcat::params::version}"
-
-  if $tomcat::params::maj_version == '6' {
+  if $version =~ /^6\./ {
     # install extra tomcat juli adapters, used to configure logging.
     class { '::tomcat::juli':
       tomcat_home => $tomcat_home,
@@ -57,14 +39,13 @@ class tomcat::source inherits tomcat::base {
     tomcat_home => $tomcat_home,
   }
 
-  $baseurl = $tomcat::params::maj_version ? {
-    '5.5' => "${tomcat::params::mirror}/tomcat-5/v${tomcat::params::version}/bin",
-    '6'   => "${tomcat::params::mirror}/tomcat-6/v${tomcat::params::version}/bin",
-  }
+  $a_version = split($version, '.')
+  $maj_version = $a_version[0]
 
-  $tomcaturl = "${baseurl}/apache-tomcat-${tomcat::params::version}.tar.gz"
+  $baseurl = "${tomcat::params::mirror}/tomcat-${maj_version}/v${version}/bin"
+  $tomcaturl = "${baseurl}/apache-tomcat-${version}.tar.gz"
 
-  archive{ "apache-tomcat-${tomcat::params::version}":
+  archive{ "apache-tomcat-${version}":
     url         => $tomcaturl,
     digest_url  => "${tomcaturl}.md5",
     digest_type => 'md5',
@@ -74,17 +55,17 @@ class tomcat::source inherits tomcat::base {
   file { '/opt/apache-tomcat':
     ensure  => link,
     target  => $tomcat_home,
-    require => Archive["apache-tomcat-${tomcat::params::version}"],
-    before  => [File['commons-logging.jar'], File['log4j.jar'], File['log4j.properties']],
+    require => Archive["apache-tomcat-${version}"],
+    before  => Class['tomcat::logging'],
   }
 
   file { $tomcat_home:
     ensure  => directory,
-    require => Archive["apache-tomcat-${tomcat::params::version}"],
+    require => Archive["apache-tomcat-${version}"],
   }
 
-    # Workarounds
-  case $tomcat::params::version {
+  # Workarounds
+  case $version {
     '6.0.18': {
       # Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=45585
       file {"${tomcat_home}/bin/catalina.sh":
@@ -94,6 +75,6 @@ class tomcat::source inherits tomcat::base {
         mode    => '0755',
       }
     }
+    default: {}
   }
-
 }
