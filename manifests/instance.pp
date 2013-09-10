e54e2aa1290932bccbd44b31ba38aff667ca723e
Cedric Jeanneret <cedric.jeanneret@camptocamp.com>
2013-09-10 11:57:17 +0200

    tomcat::instance: lint, added validate_*, ported to new module structure

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -1,125 +1,153 @@
-/*
-
-== Definition: tomcat::instance
-
-This definition will create:
-- a CATALINA_BASE directory in /srv/tomcat/$name/, readable to tomcat,
-  writeable to members of the admin group.
-- /srv/tomcat/$name/conf/{server,web}.xml which can be modified by members of
-  the admin group.
-- a /srv/tomcat/$name/webapps/ directory where administrators can drop "*.war"
-  files.
-- an init script /etc/init.d/tomcat-$name.
-- /srv/tomcat/$name/bin/setenv.sh, which is loaded by the init script.
-- /srv/tomcat/$name/bin/setenv-local.sh, which is loaded by the setenv.sh, and
-  can be edited by members of the admin group.
-- rotation of /srv/tomcat/$name/logs/catalina.out.
-
-Parameters:
-
-- *name*: the name of the instance.
-- *ensure*: defines if the state of the instance. Possible values:
-  - present: tomcat instance will be running and enabled on boot. This is the
-    default.
-  - running: an alias to "ensure => present".
-  - stopped: tomcat instance will be forced to stop, and disabled from boot,
-    but the files won't be erased from the system.
-  - installed: tomcat instance will be disabled from boot, and puppet won't
-    care if it's running or not. Useful if tomcat is managed by heartbeat.
-  - absent: tomcat instance will be stopped, disabled and completely removed
-    from the system. Warning: /srv/tomcat/$name/ will be completely erased !
-- *owner": the owner of $CATALINA_BASE/{conf,webapps}. Defaults to "tomcat".
-  Note that permissions will be different, as tomcat needs to read these
-  directories in any case.
-- *group*: the group which will be allowed to edit the instance's configuration
-  files and deploy webapps. Defaults to "adm".
-- *server_port*: tomcat's server port, defaults to 8005.
-- *http_port*: tomcat's HTTP server port, defaults to 8080.
-- *http_address*: define the IP address tomcat's HTTP server must listen on.
-  Defaults to all addresses.
-- *ajp_port*: tomcat's AJP port, defaults to 8009.
-- *ajp_address*: define the IP address tomcat's AJP server must listen on.
-  Defaults to all addresses.
-- *conf_mode*: can be used to change the permissions on
-  /srv/tomcat/$name/conf/, because some webapps require the ability to write
-  their own config files. Defaults to 2570 (writeable only by $group members).
-- *logs_mode*: can be used to change the permissions on
-  /srv/tomcat/$name/logs/, because you may need to make the logs readable
-  by other users / everyone. Defaults to 2770.
-- *server_xml_file*: can be used to set a specific server.xml file
-- *web_xml_file*: can be used to set a specific web.xml file
-- *webapp_mode*: can be used to change the permissions on
-  /srv/tomcat/$name/webapps/. Defaults to 2770 (readable and writeable by
-  $group members and tomcat himself, for auto-deploy).
-- *java_home*: can be used to define an alternate $JAVA_HOME, if you want the
-  instance to use another JVM.
-- *sample*: set to "true" and a basic "hello world" webapp will be deployed on
-  the instance. You can test it by loading this URL:
-  http://localhost:8080/sample (where 8080 is the port defined by the
-  "http_port" parameter).
-- *setenv*: optional array of environment variable definitions, which will be
-  added to setenv.sh. It will still be possible to override these variables by
-  editing setenv-local.sh.
-- *connector*: an array of tomcat::connector name (string) to include in server.xml
-- *executor*: an array of tomcat::executor name (string) to include in server.xml
-
-Requires:
-- one of the tomcat classes which installs tomcat binaries.
-- java to be previously installed.
-- logrotate to be installed.
-
-Example usage:
-
-  include tomcat
-  include tomcat::administration
-
-  tomcat::instance { "foo":
-    ensure => present,
-    group  => "tomcat-admin",
-  }
-
-  tomcat::instance { "bar":
-    ensure      => present,
-    server_port => 8006,
-    http_port   => 8081,
-    ajp_port    => 8010,
-    sample      => true,
-    setenv      => [
-      'JAVA_XMX="1200m"',
-      'ADD_JAVA_OPTS="-Xms128m"'
-    ],
-  }
-
-*/
+# == Definition: tomcat::instance
+#
+# This definition will create:
+# - a CATALINA_BASE directory in /srv/tomcat/$name/, readable to tomcat,
+#   writeable to members of the admin group.
+# - /srv/tomcat/$name/conf/{server,web}.xml which can be modified by members of
+#   the admin group.
+# - a /srv/tomcat/$name/webapps/ directory where administrators can drop "*.war"
+#   files.
+# - an init script /etc/init.d/tomcat-$name.
+# - /srv/tomcat/$name/bin/setenv.sh, which is loaded by the init script.
+# - /srv/tomcat/$name/bin/setenv-local.sh, which is loaded by the setenv.sh, and
+#   can be edited by members of the admin group.
+# - rotation of /srv/tomcat/$name/logs/catalina.out.
+#
+# Parameters:
+#
+# - *name*: the name of the instance.
+# - *ensure*: defines if the state of the instance. Possible values:
+#   - present: tomcat instance will be running and enabled on boot. This is the
+#     default.
+#   - running: an alias to "ensure => present".
+#   - stopped: tomcat instance will be forced to stop, and disabled from boot,
+#     but the files won't be erased from the system.
+#   - installed: tomcat instance will be disabled from boot, and puppet won't
+#     care if it's running or not. Useful if tomcat is managed by heartbeat.
+#   - absent: tomcat instance will be stopped, disabled and completely removed
+#     from the system. Warning: /srv/tomcat/$name/ will be completely erased !
+# - *owner": the owner of $CATALINA_BASE/{conf,webapps}. Defaults to "tomcat".
+#   Note that permissions will be different, as tomcat needs to read these
+#   directories in any case.
+# - *group*: the group which will be allowed to edit the instance's
+#   configuration files and deploy webapps. Defaults to "adm".
+# - *server_port*: tomcat's server port, defaults to 8005.
+# - *http_port*: tomcat's HTTP server port, defaults to 8080.
+# - *http_address*: define the IP address tomcat's HTTP server must listen on.
+#   Defaults to all addresses.
+# - *ajp_port*: tomcat's AJP port, defaults to 8009.
+# - *ajp_address*: define the IP address tomcat's AJP server must listen on.
+#   Defaults to all addresses.
+# - *conf_mode*: can be used to change the permissions on
+#   /srv/tomcat/$name/conf/, because some webapps require the ability to write
+#   their own config files. Defaults to 2570 (writeable only by $group members).
+# - *logs_mode*: can be used to change the permissions on
+#   /srv/tomcat/$name/logs/, because you may need to make the logs readable
+#   by other users / everyone. Defaults to 2770.
+# - *server_xml_file*: can be used to set a specific server.xml file
+# - *web_xml_file*: can be used to set a specific web.xml file
+# - *webapp_mode*: can be used to change the permissions on
+#   /srv/tomcat/$name/webapps/. Defaults to 2770 (readable and writeable by
+#   $group members and tomcat himself, for auto-deploy).
+# - *java_home*: can be used to define an alternate $JAVA_HOME, if you want the
+#   instance to use another JVM.
+# - *sample*: set to "true" and a basic "hello world" webapp will be deployed on
+#   the instance. You can test it by loading this URL:
+#   http://localhost:8080/sample (where 8080 is the port defined by the
+#   "http_port" parameter).
+# - *setenv*: optional array of environment variable definitions, which will be
+#   added to setenv.sh. It will still be possible to override these variables by
+#   editing setenv-local.sh.
+# - *connector*: an array of tomcat::connector name (string) to include in
+#   server.xml
+# - *executor*: an array of tomcat::executor name (string) to include in
+#   server.xml
+#
+# Requires:
+# - one of the tomcat classes which installs tomcat binaries.
+# - java to be previously installed.
+# - logrotate to be installed.
+#
+# Example usage:
+#
+#   include tomcat
+#   include tomcat::administration
+#
+#   tomcat::instance { "foo":
+#     ensure => present,
+#     group  => "tomcat-admin",
+#   }
+#
+#   tomcat::instance { "bar":
+#     ensure      => present,
+#     server_port => 8006,
+#     http_port   => 8081,
+#     ajp_port    => 8010,
+#     sample      => true,
+#     setenv      => [
+#       'JAVA_XMX="1200m"',
+#       'ADD_JAVA_OPTS="-Xms128m"'
+#     ],
+#   }
+#
 define tomcat::instance(
-  $ensure          = 'present',
-  $owner           = 'tomcat',
-  $group           = 'adm',
-  $server_port     = '8005',
-  $http_port       = '8080',
-  $http_address    = false,
-  $ajp_port        = '8009',
-  $ajp_address     = false,
-  $conf_mode       = '',
-  $logs_mode       = '',
-  $server_xml_file = '',
-  $web_xml_file    = '',
-  $webapp_mode     = '',
-  $java_home       = '',
-  $sample          = undef,
-  $setenv          = [],
-  $connector       = [],
-  $executor        = [],
-  $manage          = false,
-  $seluser         = undef,
-  $selrole         = undef,
-  $seltype         = undef,
-  ) {
+  $ensure           = present,
+  $owner            = 'tomcat',
+  $group            = 'adm',
+  $server_port      = '8005',
+  $http_port        = '8080',
+  $http_address     = false,
+  $ajp_port         = '8009',
+  $ajp_address      = false,
+  $conf_mode        = '',
+  $logs_mode        = '',
+  $server_xml_file  = '',
+  $web_xml_file     = '',
+  $webapp_mode      = '',
+  $java_home        = '',
+  $sample           = undef,
+  $setenv           = [],
+  $connector        = [],
+  $executor         = [],
+  $manage           = false,
+  $seluser          = 'system_u',
+  $selrole          = 'object_r',
+  $seltype          = 'initrc_exec_t',
+  $instance_basedir = false,
+  $tomcat_version   = false,
+) {
 
-  include tomcat::params
+  validate_re($ensure, [
+    'present',
+    'running',
+    'stopped',
+    'installed',
+    'absent'
+    ])
+  validate_string($owner)
+  validate_string($group)
+  validate_re($server_port, '^[0-9]+$')
+  validate_re($http_port, '^[0-9]+$')
+  validate_re($ajp_port, '^[0-9]+$')
+  validate_array($setenv)
+  validate_array($connector)
+  validate_array($executor)
+  validate_bool($manage)
+
+  include ::tomcat::params
+  $_basedir = $instance_basedir? {
+    false   => tomcat::params::instance_basedir,
+    default => $instance_basedir,
+  }
+  validate_absolute_path($_basedir)
+  $version = $tomcat_version? {
+    false   => $tomcat::params::version,
+    default => $tomcat_version,
+  }
+  validate_re($version, '^[5-7]$')
 
   $tomcat_name = $name
-  $basedir = "${tomcat::params::instance_basedir}/${name}"
+  $basedir = "${_basedir}/${name}"
 
   if $owner == 'tomcat' {
     $dirmode  = $webapp_mode ? {
@@ -152,15 +180,23 @@ define tomcat::instance(
     }
   }
 
+  validate_re($dirmode, '^[0-9]+$')
+  validate_re($filemode, '^[0-9]+$')
+  validate_re($confmode, '^[0-9]+$')
+  validate_re($logsmode, '^[0-9]+$')
+
+
   if $connector == [] and $server_xml_file == '' {
 
     $connectors = ["http-${http_port}-${name}","ajp-${ajp_port}-${name}"]
 
+    $connector_ensure = $ensure? {
+      absent  => absent,
+      default => present,
+    }
+
     tomcat::connector{"http-${http_port}-${name}":
-      ensure   => $ensure ? {
-        'absent' => absent,
-        default  => present,
-      },
+      ensure   => $connector_ensure,
       instance => $name,
       protocol => 'HTTP/1.1',
       port     => $http_port,
@@ -171,10 +207,7 @@ define tomcat::instance(
     }
 
     tomcat::connector{"ajp-${ajp_port}-${name}":
-      ensure   => $ensure ? {
-        'absent' => absent,
-        default  => present,
-      },
+      ensure   => $connector_ensure,
       instance => $name,
       protocol => 'AJP/1.3',
       port     => $ajp_port,
@@ -188,41 +221,31 @@ define tomcat::instance(
     $connectors = $connector
   }
 
-  if defined(File[$tomcat::params::instance_basedir]) {
-    debug "File[${tomcat::params::instance_basedir}] already defined"
-  } else {
-    file {$tomcat::params::instance_basedir:
-      ensure => directory,
-    }
-  }
-
-  if $tomcat::params::type == 'package' and $::osfamily == 'RedHat' and $::operatingsystemrelease =~ /^6.*/ {
-    # force catalina.sh to use the common library in CATALINA_HOME and not CATALINA_BASE
+  if $tomcat::params::type == 'package' and
+      $::osfamily == 'RedHat' and
+      $::operatingsystemrelease =~ /^6.*/ {
+    # force catalina.sh to use the common library
+    # in CATALINA_HOME and not CATALINA_BASE
     $classpath = '/usr/share/tomcat6/bin/tomcat-juli.jar'
   }
 
   # default server.xml is slightly different between tomcat5.5 and tomcat6
-  if $tomcat::params::maj_version == '5.5' {
-    $serverdotxml = 'server.xml.tomcat55.erb'
+  $serverdotxml = $version? {
+    5 => 'server.xml.tomcat55.erb',
+    6 => 'server.xml.tomcat6.erb',
+    7 => 'server.xml.tomcat7.erb',
   }
 
-  if $tomcat::params::maj_version == '6' {
-    $serverdotxml = 'server.xml.tomcat6.erb'
-  }
-
-  if $tomcat::params::maj_version == '5.5' and $tomcat::params::type == 'package' {
-    $catalinahome = $::osfamily ? {
-      RedHat => '/usr/share/tomcat5',
-      Debian => '/usr/share/tomcat5.5',
+  if $tomcat::params::type == 'package' {
+    $catalinahome = $version? {
+      5        => $::osfamily? {
+        RedHat => '/usr/share/tomcat5',
+        Debian => '/usr/share/tomcat5.5',
+      },
+      default  => "/usr/share/tomcat${version}",
     }
   }
 
-  if $tomcat::params::maj_version == '6' and $tomcat::params::type == 'package' {
-    $catalinahome = $::osfamily ? {
-      RedHat => '/usr/share/tomcat6',
-      Debian => '/usr/share/tomcat6',
-    }
-  }
 
   # In this case, we are using a non package-based tomcat.
   if $tomcat::params::type == 'source' {
@@ -230,7 +253,7 @@ define tomcat::instance(
   }
 
   # Define a version string for use in templates
-  $tomcat_version_str = "${tomcat::params::maj_version}_${tomcat::params::type}"
+  $tomcat_version_str = "${version}_${tomcat::params::type}"
 
   # Define default JAVA_HOME used in tomcat.init.erb
   if $java_home == '' {
@@ -254,6 +277,7 @@ define tomcat::instance(
   } else {
     $javahome = $java_home
   }
+  validate_absolute_path($javahome)
 
   # Instance directories
   case $ensure {
@@ -310,7 +334,7 @@ define tomcat::instance(
             default => $server_xml_file,
           },
           content => $server_xml_file? {
-            ''      => template("tomcat/${serverdotxml}"),
+            ''      => template("${module_name}/${serverdotxml}"),
             default => undef,
           },
           before  => Service["tomcat-${name}"],
@@ -334,7 +358,7 @@ define tomcat::instance(
             default => $web_xml_file,
           },
           content => $web_xml_file? {
-            ''      => template('tomcat/web.xml.erb'),
+            ''      => template("${module_name}/web.xml.erb"),
             default => undef,
           },
           before  => Service["tomcat-${name}"],
@@ -349,7 +373,7 @@ define tomcat::instance(
           owner   => 'root',
           group   => 'root',
           mode    => '0644',
-          content => template('tomcat/README.erb');
+          content => template("${module_name}/README.erb");
 
         "${basedir}/webapps":
           ensure => directory,
@@ -389,7 +413,7 @@ define tomcat::instance(
           owner   => 'tomcat',
           group   => $group,
           mode    => '0460',
-          source  => 'puppet:///modules/tomcat/sample.war',
+          source  => "puppet:///modules/${module_name}/sample.war",
           require => File["${basedir}/webapps"],
           before  => Service["tomcat-${name}"],
         }
@@ -400,6 +424,7 @@ define tomcat::instance(
         ensure  => absent,
         recurse => true,
         force   => true,
+        require => Service["tomcat-${name}"],
       }
     }
   }
@@ -416,7 +441,7 @@ define tomcat::instance(
   # Default JVM options
   file {"${basedir}/bin/setenv.sh":
     ensure  => $present,
-    content => template('tomcat/setenv.sh.erb'),
+    content => template("${module_name}/setenv.sh.erb"),
     owner   => 'root',
     group   => $group,
     mode    => '0754',
@@ -442,22 +467,13 @@ define tomcat::instance(
     owner   => 'root',
     mode    => '0755',
     require => File["${basedir}/bin/setenv.sh"],
-    seluser => $seluser ? {
-            ''      => 'system_u',
-            default => $seluser,
-          },
-    selrole => $selrole ? {
-            ''      => 'object_r',
-            default => $selrole,
-          },
-    seltype => $seltype ? {
-            ''      => 'initrc_exec_t',
-            default => $seltype,
-          },
+    seluser => $seluser,
+    selrole => $selrole,
+    seltype => $seltype,
   }
 
   if $tomcat::params::type == 'package' {
-    $servicerequire = Package['tomcat']
+    $servicerequire = Class['tomcat']
   } else {
     $servicerequire = File['/opt/apache-tomcat']
   }
