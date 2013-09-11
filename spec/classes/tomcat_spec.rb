b186e336375e928bfc22119e0018bddd486e540b
Cedric Jeanneret <cedric.jeanneret@camptocamp.com>
2013-09-11 17:06:21 +0200

    classes unit-tests - not finished for now

---
+++ spec/classes/tomcat_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+require File.expand_path(File.dirname(__FILE__)) + '/../defines/parameters.rb'
+
+deb6 = @parameters['DEB6']
+deb7 = @parameters['DEB7']
+rhel5 = @parameters['RHEL5']
+rhel6 = @parameters['RHEL6']
+
+describe "tomcat" do
+end
