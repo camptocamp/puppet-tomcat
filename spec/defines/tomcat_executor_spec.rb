d97b920b7ae20e4a45328d5d8282ab8c64b34b4e
Cedric Jeanneret <cedric.jeanneret@camptocamp.com>
2013-09-11 08:07:58 +0200

    finished unit-tests for tomcat::executor

--- spec/defines/tomcat_executor_spec.rb
+++ spec/defines/tomcat_executor_spec.rb
@@ -20,5 +20,97 @@ require File.expand_path(File.direname(__FILE__)) + '/parameters.rb'
         }.to raise_error(Puppet::ParseError, /validate_re\(\):/)
       end
     end
+
+    context 'when using a wrong owner value' do
+      let (:params) {{
+        :ensure   => 'present',
+        :instance => 'instance',
+        :owner    => true
+      }}
+      it 'should fail' do
+        expect { 
+          should contain_tomcat__executor('executorBar')
+        }.to raise_error(Puppet::ParseError, /.+ is not a string/)
+      end
+    end
+
+    context 'when using a wrong group value' do
+      let (:params) {{
+        :ensure   => 'present',
+        :instance => 'instance',
+        :group    => true
+      }}
+      it 'should fail' do
+        expect { 
+          should contain_tomcat__executor('executorBar')
+        }.to raise_error(Puppet::ParseError, /.+ is not a string/)
+      end
+    end
+
+    context 'when using a wrong daemon value' do
+      let (:params) {{
+        :ensure   => 'present',
+        :instance => 'instance',
+        :daemon   => 'foobar'
+      }}
+      it 'should fail' do
+        expect { 
+          should contain_tomcat__executor('executorBar')
+        }.to raise_error(Puppet::ParseError, /.+ is not a boolean/)
+      end
+    end
+
+    context 'when using a wrong max_threads value' do
+      let (:params) {{
+        :ensure   => 'present',
+        :instance => 'instance',
+        :max_threads => 'string'
+      }}
+      it 'should fail' do
+        expect { 
+          should contain_tomcat__executor('executorBar')
+        }.to raise_error(Puppet::ParseError, /validate_re\(\)/)
+      end
+    end
+
+    context 'when using a wrong min_spare_threads value' do
+      let (:params) {{
+        :ensure   => 'present',
+        :instance => 'instance',
+        :min_spare_threads => 'string'
+      }}
+      it 'should fail' do
+        expect { 
+          should contain_tomcat__executor('executorBar')
+        }.to raise_error(Puppet::ParseError, /validate_re\(\)/)
+      end
+    end
+
+    context 'when using a wrong max_idle_time value' do
+      let (:params) {{
+        :ensure   => 'present',
+        :instance => 'instance',
+        :max_idle_time => 'string'
+      }}
+      it 'should fail' do
+        expect { 
+          should contain_tomcat__executor('executorBar')
+        }.to raise_error(Puppet::ParseError, /validate_re\(\)/)
+      end
+
+    context 'when using a wrong manage value' do
+      let (:params) {{
+        :ensure   => 'present',
+        :instance => 'instance',
+        :manage   => 'bbbb'
+      }}
+      it 'should fail' do
+        expect { 
+          should contain_tomcat__executor('executorBar')
+        }.to raise_error(Puppet::ParseError, /.+ is not a boolean/)
+      end
+    end
+    end
+
   end
 }
