f8bf6528a0671c84cc9064ea1a0afeb13dbd7fd6
Cedric Jeanneret <cedric.jeanneret@camptocamp.com>
2013-09-12 12:16:48 +0200

    default_include spec is now more clear for debug

--- spec/hosts/default_include_spec.rb
+++ spec/hosts/default_include_spec.rb
@@ -3,7 +3,7 @@ require File.expand_path(File.dirname(__FILE__)) + '/../defines/parameters.rb'
 
 @parameters.each { |k, v|
 
-  describe 'default' do
+  describe 'default_include' do
 
     let (:facts) {{
       :osfamily                  => v['osfamily'],
@@ -12,27 +12,27 @@ require File.expand_path(File.dirname(__FILE__)) + '/../defines/parameters.rb'
       :lsbdistmajrelease         => v['lsbdistmajrelease'],
     }}
 
-    describe "should install #{v['tomcat_package']} package" do
+    describe "#{k}: should install #{v['tomcat_package']} package" do
       it {
         should contain_package(v['tomcat_package'])
       }
     end
 
-    describe "should install #{v['log4j']} and #{v['logging']}" do
+    describe "#{k}: should install #{v['log4j']} and #{v['logging']}" do
       it {
         should contain_package(v['log4j'])
         should contain_package(v['logging'])
       }
     end
 
-    describe "should contain tomcat::connectors" do
+    describe "#{k}: should contain tomcat::connectors" do
       it {
         should contain_tomcat__connector('http-8080-instance1').with_ensure('present')
         should contain_tomcat__connector('ajp-8009-instance1').with_ensure('present')
       }
     end
 
-    describe "should have tomcat-instance1 server" do
+    describe "#{k}: should have tomcat-instance1 server" do
       it {
         should contain_service('tomcat-instance1').with({
           'ensure' => 'running',
