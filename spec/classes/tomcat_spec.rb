7443d56b8ed377d1851ba78da7bb3f858ddcb024
Cedric Jeanneret <cedric.jeanneret@camptocamp.com>
2013-09-12 12:15:44 +0200

    added unit-tests for tomcat main class

--- spec/classes/tomcat_spec.rb
+++ spec/classes/tomcat_spec.rb
@@ -1,10 +1,101 @@
 require 'spec_helper'
 require File.expand_path(File.dirname(__FILE__)) + '/../defines/parameters.rb'
 
-deb6 = @parameters['DEB6']
-deb7 = @parameters['DEB7']
-rhel5 = @parameters['RHEL5']
-rhel6 = @parameters['RHEL6']
+@parameters.each { |k,v|
+  describe "tomcat" do
+    context k do
+      let (:facts) { {
+        :osfamily                  => v['osfamily'],
+        :operatingsystem           => v['operatingsystem'],
+        :operatingsystemmajrelease => v['operatingsystemmajrelease'],
+        :lsbdistmajrelease         => v['lsbdistmajrelease'],
+      } }
 
-describe "tomcat" do
-end
+      describe 'should include basic classes' do
+        it {
+          should include_class('tomcat::install')
+          should include_class('tomcat::user')
+          should include_class('tomcat::service')
+        }
+      end
+
+      describe 'should install tomcat package' do
+        it {
+          should contain_package(v['tomcat_package'])
+        }
+      end
+
+      describe 'should include tomcat::logging and tomcat::juli' do
+        it {
+          should include_class('tomcat::logging')
+          should include_class('tomcat::juli')
+        }
+      end
+
+      describe "should install #{v['log4j']} and #{v['logging']}" do
+        it {
+          should contain_package(v['log4j'])
+          should contain_package(v['logging'])
+        }
+      end
+
+      describe 'should create tomcat user' do
+        it {
+          should contain_user('tomcat').with('system' => true)
+        }
+      end
+
+      describe 'should deactivate default tomcat service' do
+        it {
+          should contain_service("tomcat#{v['tomcat_version']}").with({
+            'ensure' => 'stopped',
+            'enable' => false,
+          })
+          should contain_file("/etc/init.d/tomcat#{v['tomcat_version']}").with({
+            'ensure' => 'file',
+            'mode'   => '0644',
+          })
+        }
+      end
+
+      {
+        '5.0.1' => 5,
+        '6.0.1' => 6,
+        '7.0.1' => 7,
+      }.each { |version, maj|
+        context "install tomcat#{version} from sources" do
+          let(:params) {{
+            :sources => true,
+            :version => version,
+          }}
+          describe 'should include tomcat::source' do
+            it {
+              should include_class('tomcat::source')
+            }
+          end
+          describe 'should download tomcat' do
+            it {
+              should contain_archive("apache-tomcat-#{version}").with({
+                'url' => /tomcat-#{maj}\/v#{version}\/bin/,
+              })
+            }
+          end
+          describe 'should create tomcat home' do
+            it {
+              should contain_file("/opt/apache-tomcat-#{version}").with_ensure('directory')
+            }
+          end
+          describe 'should create symlink' do
+            it {
+              should contain_file('/opt/apache-tomcat').with({
+                'ensure'  => 'link',
+                'target'  => "/opt/apache-tomcat-#{version}",
+              })
+            }
+          end
+        end
+      }
+
+    end
+  end
+}
