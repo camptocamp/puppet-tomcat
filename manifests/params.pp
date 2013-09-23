c5f1a1085de8a8afede0b007b04907f25c67daff
Cedric Jeanneret <cedric.jeanneret@camptocamp.com>
2013-09-23 15:10:05 +0200

    it seems we have to use hiera() in here if we want to fixe the version — else it may not detect correctly what we really want to do :(

--- manifests/params.pp
+++ manifests/params.pp
@@ -6,17 +6,18 @@ class tomcat::params {
 
   $uid = undef
   $gid = undef
+  $_version = hiera('tomcat::version', $version)
 
   # UGLY hack for juli/logging [debian only for what I can see]
-  $src_version = $version? {
+  $src_version = $_version? {
     5 => '5.5.27',
     6 => '6.0.26',
     7 => '7.0.42',
   }
 
   $home = $::osfamily? {
-    Debian => "/usr/share/tomcat${version}",
-    RedHat => "/var/lib/tomcat${version}",
+    Debian => "/usr/share/tomcat${_version}",
+    RedHat => "/var/lib/tomcat${_version}",
   }
 
   $instance_basedir = '/srv/tomcat'
