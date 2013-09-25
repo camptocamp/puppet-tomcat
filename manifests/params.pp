b7380f2b031f8c6162a89d7bb2d5071c136fb729
Christian Kaenzig <christian.kaenzig@camptocamp.com>
2013-09-25 11:03:36 +0200

    params fixes
    - remove call to hiera
    - explicit version list on redhat
    - remove temp var $_version

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,23 +1,24 @@
 class tomcat::params {
   $version = $::osfamily? {
-    Debian => 6,
-    RedHat => $::lsbdistmajrelease,
+    Debian => '6',
+    RedHat => $::lsbdistmajrelease ? {
+      '5' => '5',
+      '6' => '6',
+    }
   }
 
   $uid = undef
   $gid = undef
-  $_version = hiera('tomcat::version', $version)
 
-  # UGLY hack for juli/logging [debian only for what I can see]
-  $src_version = $_version? {
+  $src_version = $version? {
     5 => '5.5.27',
     6 => '6.0.26',
     7 => '7.0.42',
   }
 
   $home = $::osfamily? {
-    Debian => "/usr/share/tomcat${_version}",
-    RedHat => "/var/lib/tomcat${_version}",
+    Debian => "/usr/share/tomcat${version}",
+    RedHat => "/var/lib/tomcat${version}",
   }
 
   $instance_basedir = '/srv/tomcat'
