67299134b18091e4fac2582646d882d1d54dbdb1
Maxence Dunnewind <maxence.dunnewind@inria.fr>
2013-10-02 10:33:57 +0200

    add support for tomcat7

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -210,6 +210,10 @@ define tomcat::instance(
     $serverdotxml = 'server.xml.tomcat6.erb'
   }
 
+  if $tomcat::params::maj_version == '7' {
+    $serverdotxml = 'server.xml.tomcat7.erb'
+  }
+
   if $tomcat::params::maj_version == '5.5' and $tomcat::params::type == 'package' {
     $catalinahome = $::osfamily ? {
       RedHat => '/usr/share/tomcat5',
