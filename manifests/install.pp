c3ad985732d74754211568be6fa8a3ad36d8ad77
Amos Wood <amos.wood@lifeway.com>
2014-07-16 15:09:34 -0500

    init tomcat7 service start-up fix

--- manifests/install.pp
+++ manifests/install.pp
@@ -18,14 +18,16 @@ class tomcat::install {
     # Moved from Class[tomcat::service] to here so that we can create a
     # tomcat::service definition.
     file {"/etc/init.d/tomcat${tomcat::version}":
-      ensure => file,
-      mode   => '0644',
-    } ->
+      ensure  => file,
+      mode    => '0644',
+      require => Package["tomcat${tomcat::version}"],
+    }
     service {"tomcat${tomcat::version}":
       ensure => stopped,
       enable => false,
+      require => File["/etc/init.d/tomcat${tomcat::version}"],
     }
-
+    
   } else {
     class {'tomcat::source': }
   }
