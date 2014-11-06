4da668a5fabc71fb68269260e617d60d45fe636d
Raphaël Pinson <raphael.pinson@camptocamp.com>
2014-11-06 10:36:32 +0100

    Try RH7 like RH6

--- manifests/install/redhat.pp
+++ manifests/install/redhat.pp
@@ -5,7 +5,7 @@
 class tomcat::install::redhat {
 
   case $::operatingsystemmajrelease {
-    '5', '7': {
+    '5': {
       file {'/usr/share/tomcat5/bin/catalina.sh':
         ensure  => link,
         target  => "/usr/bin/dtomcat${tomcat::version}",
@@ -13,7 +13,7 @@ class tomcat::install::redhat {
       }
     }
 
-    '6': {
+    '6', '7': {
       file {"/usr/share/tomcat${tomcat::version}/bin/setclasspath.sh":
         ensure  => file,
         owner   => root,
