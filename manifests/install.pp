cff3bcfb8ed541090298b5ab148d7b7c6c2d8c98
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-11-13 10:02:22 +0100

    Don't configure juli and logging on RedHat7

--- manifests/install.pp
+++ manifests/install.pp
@@ -12,26 +12,41 @@ class tomcat::install {
     'debian' => "tomcat${tomcat::version}",
   }
 
+  $service_name = $::osfamily ? {
+    'RedHat' => $::operatingsystemmajrelease ? {
+      '7'     => 'tomcat',
+      default => "tomcat${tomcat::version}",
+    },
+    'Debian' => "tomcat${tomcat::version}",
+  }
+
   if !$tomcat::sources {
     package {'tomcat':
       ensure => present,
       name   => $package_name,
-    } ->
-    class {'::tomcat::juli': } ->
-    class {'::tomcat::logging': }
-
-    if $::osfamily == 'RedHat' {
-      class {'::tomcat::install::redhat': }
     }
 
-    # Moved from Class[tomcat::service] to here so that we can create a
-    # tomcat::service definition.
-    file {"/etc/init.d/tomcat${tomcat::version}":
-      ensure => file,
-      mode   => '0644',
-    } ->
-    service {"tomcat${tomcat::version}":
+    if $::osfamily != 'RedHat' or $::operatingsystemmajrelease != 7 {
+      Package['tomcat'] ->
+      class {'::tomcat::juli': } ->
+      class {'::tomcat::logging': }
+
+      if $::osfamily == 'RedHat' {
+        class {'::tomcat::install::redhat': }
+      }
+
+      file {"/etc/init.d/tomcat${tomcat::version}":
+        ensure  => file,
+        mode    => '0644',
+        require => Package['tomcat'],
+        before  => Service['tomcat'],
+      }
+    }
+
+    # Ensure default service is stopped
+    service { 'tomcat':
       ensure => stopped,
+      name   => $service_name,
       enable => false,
     }
 
