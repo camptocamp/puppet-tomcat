a234699d006c9435d6cd93a73d0405051a0cc1a1
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-11-14 08:47:52 +0100

    Add acceptance tests

--- spec/acceptance/instance_spec.rb
+++ spec/acceptance/instance_spec.rb
@@ -25,4 +25,61 @@ describe 'tomcat::instance' do
       it { is_expected.to be_listening }
     end
   end
+
+  context 'with another http port' do
+    it 'should idempotently run' do
+      pp = <<-EOS
+        class { 'tomcat': }
+        tomcat::instance { 'foo':
+          http_port => '8081',
+        }
+      EOS
+
+      apply_manifest(pp, :catch_failures => true)
+      apply_manifest(pp, :catch_changes => true)
+    end
+
+    describe port(8005) do
+      it { is_expected.to be_listening }
+    end
+
+    describe port(8009) do
+      it { is_expected.to be_listening }
+    end
+
+    describe port(8080) do
+      it { should_not be_listening }
+    end
+
+    describe port(8081) do
+      it { is_expected.to be_listening }
+    end
+  end
+
+  context 'with an env variable set' do
+    it 'should idempotently run' do
+      pp = <<-EOS
+        class { 'tomcat': }
+        tomcat::instance { 'foo':
+          setenv => ['USE_IMAGEMAGICK="true"',],
+        }
+      EOS
+
+      apply_manifest(pp, :catch_failures => true)
+      apply_manifest(pp, :catch_changes => true)
+    end
+
+    describe port(8005) do
+      it { is_expected.to be_listening }
+    end
+
+    describe port(8009) do
+      it { is_expected.to be_listening }
+    end
+
+    describe port(8080) do
+      it { is_expected.to be_listening }
+    end
+  end
+
 end
