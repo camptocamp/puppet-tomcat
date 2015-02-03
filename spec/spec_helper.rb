f16079a811b12511b9933014f90e6952712d8db1
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-02-03 14:20:25 +0100

    Speed up unit tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,24 +7,11 @@ RSpec.configure do |c|
   c.include PuppetlabsSpec::Files
 
   c.before :each do
-    # Ensure that we don't accidentally cache facts and environment
-    # between test cases.
-    Facter::Util::Loader.any_instance.stubs(:load_all)
-    Facter.clear
-    Facter.clear_messages
-
     # Store any environment variables away to be restored later
     @old_env = {}
     ENV.each_key {|k| @old_env[k] = ENV[k]}
 
-    if Gem::Version.new(`puppet --version`) >= Gem::Version.new('3.5')
-      Puppet.settings[:strict_variables]=true
-    end
-
-    if ENV['FUTURE_PARSER'] == 'yes'
-      c.parser='future'
-    end
-
+    Puppet.settings[:strict_variables]=true if Gem::Version.new(Puppet::PUPPETVERSION) >= Gem::Version.new('3.5')
     Puppet.features.stubs(:root?).returns(true)
   end
 
