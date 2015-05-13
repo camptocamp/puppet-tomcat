23efd67537619b0797985714b2af26eded10c2c1
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-05-13 15:06:21 +0200

    Remove unused defines tomcat::connector::option and tomcat::env to ease migration to puppetlabs-concat

--- manifests/connector/option.pp
+++
@@ -1,9 +0,0 @@
-define tomcat::connector::option(
-  $connector,
-  $key,
-  $value,
-) {
-  concat_fragment { "connector_${connector}+10_${name}":
-    content => "           ${key}=\"${value}\"",
-  }
-}
--- manifests/env.pp
+++
@@ -1,9 +0,0 @@
-define tomcat::env(
-  $instance,
-  $key,
-  $value,
-) {
-  concat_fragment { "setenv.sh_${instance}+02_${name}":
-    content => "export ${key}=\"${value}\"",
-  }
-}
