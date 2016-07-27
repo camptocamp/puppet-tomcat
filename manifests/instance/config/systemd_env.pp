9a8864c4f93e8ebd8a780ffa74b556be34c099d4
Christian Kaenzig <christian.kaenzig@camptocamp.com>
2016-07-27 15:40:23 +0200

    Fix bug in RHEL7 environment settings

--- manifests/instance/config/systemd_env.pp
+++ manifests/instance/config/systemd_env.pp
@@ -6,7 +6,7 @@ define tomcat::instance::config::systemd_env (
   $split_env = split($title, '=')
   $env_name = $split_env[0]
 
-  $env_val = inline_template('<%= @split_env[1][0] == \'"\' and @split_env[1][-1] == \'"\' ? @split_env[1].slice(1..-2) : @split_env[1] %>')
+  $env_val = inline_template('<%= (@split_env[1][0] == \'"\' and @split_env[1][-1] == \'"\') ? @split_env[1].slice(1..-2) : @split_env[1] %>')
 
   shellvar {"${env_name}_${instance}":
     ensure    => present,
