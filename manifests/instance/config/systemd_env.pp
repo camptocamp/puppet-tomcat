104a2181db6654c6de3d8eb4aa819236e2645d7e
Christian Kaenzig <christian.kaenzig@camptocamp.com>
2016-07-28 12:01:38 +0200

    Fix bug again in RHEL7 environment settings
    Was failing when a '=' present in a setting value.

--- manifests/instance/config/systemd_env.pp
+++ manifests/instance/config/systemd_env.pp
@@ -6,7 +6,8 @@ define tomcat::instance::config::systemd_env (
   $split_env = split($title, '=')
   $env_name = $split_env[0]
 
-  $env_val = inline_template('<%= (@split_env[1][0] == \'"\' and @split_env[1][-1] == \'"\') ? @split_env[1].slice(1..-2) : @split_env[1] %>')
+  $raw_env_val = inline_template('<%= (@split_env[1..-1]).join("=") %>')
+  $env_val = inline_template('<%= (@raw_env_val[0] == \'"\' and @raw_env_val[-1] == \'"\') ? @raw_env_val.slice(1..-2) : @raw_env_val %>')
 
   shellvar {"${env_name}_${instance}":
     ensure    => present,
