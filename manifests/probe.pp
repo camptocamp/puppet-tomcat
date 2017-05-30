ab26b9c90a5150cc83bf488ddd9301553c03de47
Simon Séhier <simon.sehier@camptocamp.com>
2017-05-30 13:50:22 +0200

    remove parameters to pass tests

--- manifests/probe.pp
+++ manifests/probe.pp
@@ -48,11 +48,11 @@ define tomcat::probe($ensure='present', $version='2.0.4') {
     url           => $url,
     digest_string => $sha1sum,
     digest_type   => 'sha1',
-    extension     => 'zip',
+    #extension     => 'zip',
     target        => "/usr/src/psi-probe-${version}",
     # hack to avoid the exec reexecuting always, as the zip file contains no
     # base directory.
-    root_dir      => 'probe.war',
+    #root_dir      => 'probe.war',
   }
 
   file { "/srv/tomcat/${name}/webapps/probe.war":
