8178fe4f2ae5a98cbfd8b99ae2cf43269511eb70
Diego Abelenda <diego.abelenda@camptocamp.com>
2018-05-18 12:33:44 +0200

    Add tests for archive integration

--- spec/acceptance/tomcat_spec.rb
+++ spec/acceptance/tomcat_spec.rb
@@ -12,4 +12,19 @@ describe 'tomcat' do
       apply_manifest(pp, :catch_changes => true)
     end
   end
+
+  context 'with source' do
+    it 'should idempotently run' do
+      pp = <<-EOS
+        class { 'tomcat':
+          sources    => true,
+          srcversion => '6.0.41',
+          version    => '6',
+        }
+      EOS
+
+      apply_manifest(pp, :catch_failures => true)
+      apply_manifest(pp, :catch_changes => true)
+    end
+  end
 end
